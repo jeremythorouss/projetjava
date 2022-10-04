6d18bade03cff173aa74b6c0077b62eca2255705
jeremythorouss <jeremy@thorouss.xyz>
2022-10-04 09:41:14 +0200

    on a avance mais on ne voit toujours pas les tables

--- api/initdb/1_TABLES.sql
+++ api/initdb/1_TABLES.sql
@@ -1,9 +1,11 @@
+Drop table  users
+
 create table users
 (
     id bigint auto_increment,
     constraint users_pk
         primary key (id),
-    Name TEXT not null,
-    Email TEXT not null,
+    Name varchar(255) not null,
+    Email varchar(255) not null,
     Birthdate datetime null
 );
